3ce0fc439ca0ef9cd8f65393901469d2196b41e9
joao-gregorini <joaovitorgregorini@gmail.com>
2026-08-27 17:55:47 -0300

    Update book.entity.ts

--- filefront/src/Data/Entity/book.entity.ts
+++ filefront/src/Data/Entity/book.entity.ts
@@ -1,5 +1,16 @@
+import type { AuthorEntity } from "./author.entity";
+
 export type BookEntity = {
     Id:number,
     Name:string,
-    CurrentChapter:number
+    Summary:string,
+    CurrentChapter?:number,
+    TotalChapters?:number,
+    Rating?:number,
+    Words?:number,
+    ReadingStatus?:number,
+    WritingStatus?:number,
+    Author?:AuthorEntity[],
+    URLS?:any[],
+    Files?:any[],
 }
